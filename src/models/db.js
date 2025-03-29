b0c6fb6b0120c8fa82adcf6dfe89ce1377d3d970
zamaraev96 <m.zamaraev96@gmail.com>
2025-03-29 09:39:27 +0300

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '89031823657',
+  password: '',
   database: 'warehouse'
 });
 
